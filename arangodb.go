c81d24447086e74ba9d11280e675486379cd05e0
Anatoliy Hanukaev <87340160+AnatoliyHanukaev@users.noreply.github.com>
2021-07-16 13:15:59 -0700

    Added CookieConnection
    Added Cookie Connection and allowed user to set default database permissions.

--- arangodb.go
+++ arangodb.go
@@ -4,12 +4,13 @@ import (
 	"context"
 	"crypto/tls"
 	"fmt"
+	"strings"
+	"sync"
+
 	"github.com/arangodb/go-driver"
 	"github.com/arangodb/go-driver/http"
 	"github.com/hashicorp/vault/sdk/database/dbplugin/v5"
 	"github.com/hashicorp/vault/sdk/database/helper/credsutil"
-	"strings"
-	"sync"
 )
 
 var _ dbplugin.Database = (*ArangoDB)(nil)
@@ -40,7 +41,7 @@ func (db *ArangoDB) Initialize(ctx context.Context, req dbplugin.InitializeReque
 
 	if req.VerifyConnection {
 		if _, err := newClient.Users(ctx); err != nil {
-			return dbplugin.InitializeResponse{}, err
+			return dbplugin.InitializeResponse{}, fmt.Errorf("failed to verify connection: %w", err)
 		}
 	}
 
@@ -79,15 +80,12 @@ func (db *ArangoDB) NewUser(ctx context.Context, req dbplugin.NewUserRequest) (d
 		Password: req.Password,
 	})
 	if err != nil {
-		return dbplugin.NewUserResponse{}, err
+		return dbplugin.NewUserResponse{}, fmt.Errorf("failed to create new user: %w", err)
 	}
 
 	if err := db.grantPermissions(ctx, user, permissions); err != nil {
 		// TODO: not really sure what to do in these cases, for now let's just try cleaning up
-		if err := user.Remove(ctx); err != nil {
-			return dbplugin.NewUserResponse{}, fmt.Errorf("error while removing failed user: %w", err)
-		}
-
+		user.Remove(ctx)
 		return dbplugin.NewUserResponse{}, err
 	}
 
@@ -105,14 +103,14 @@ func (db *ArangoDB) UpdateUser(ctx context.Context, req dbplugin.UpdateUserReque
 
 	user, err := db.client.User(ctx, req.Username)
 	if err != nil {
-		return dbplugin.UpdateUserResponse{}, err
+		return dbplugin.UpdateUserResponse{}, fmt.Errorf("failed to read user data: %w", err)
 	}
 
 	if req.Password != nil {
 		if err := user.Update(ctx, driver.UserOptions{
 			Password: req.Password.NewPassword,
 		}); err != nil {
-			return dbplugin.UpdateUserResponse{}, err
+			return dbplugin.UpdateUserResponse{}, fmt.Errorf("failed to update password: %w", err)
 		}
 	}
 
@@ -132,12 +130,12 @@ func (db *ArangoDB) DeleteUser(ctx context.Context, req dbplugin.DeleteUserReque
 			return dbplugin.DeleteUserResponse{}, nil
 		}
 
-		return dbplugin.DeleteUserResponse{}, err
+		return dbplugin.DeleteUserResponse{}, fmt.Errorf("failed to read user data: %w", err)
 	}
 
 	err = user.Remove(ctx)
 	if err != nil && !driver.IsNotFound(err) {
-		return dbplugin.DeleteUserResponse{}, err
+		return dbplugin.DeleteUserResponse{}, fmt.Errorf("failed to remove user: %w", err)
 	}
 
 	return dbplugin.DeleteUserResponse{}, nil
@@ -151,7 +149,7 @@ func (db *ArangoDB) Close() error {
 func (db *ArangoDB) createClient(raw map[string]interface{}) (driver.Client, error) {
 	config, err := configFromRaw(raw)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to parse config data: %w", err)
 	}
 
 	var tlsConfig *tls.Config
@@ -166,15 +164,15 @@ func (db *ArangoDB) createClient(raw map[string]interface{}) (driver.Client, err
 		TLSConfig: tlsConfig,
 	})
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to create new connection: %w", err)
 	}
 
 	client, err := driver.NewClient(driver.ClientConfig{
-		Connection:     conn,
+		Connection:     &CookieConnection{Connection: conn},
 		Authentication: driver.BasicAuthentication(config.Username, config.Password),
 	})
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to create new client: %w", err)
 	}
 
 	return client, nil
@@ -182,26 +180,63 @@ func (db *ArangoDB) createClient(raw map[string]interface{}) (driver.Client, err
 
 func (db *ArangoDB) grantPermissions(ctx context.Context, user driver.User, permissions []Permission) error {
 	for _, permission := range permissions {
-		database, err := db.client.Database(ctx, permission.Database)
-		if err != nil {
-			return err
+		var database driver.Database
+		if len(permission.Database) > 0 && permission.Database != "*" {
+			pdb, err := db.client.Database(ctx, permission.Database)
+			if err != nil {
+				return fmt.Errorf("failed to read database data: %w", err)
+			}
+			database = pdb
 		}
 
 		if permission.Collection == "" || permission.Collection == "*" {
 			if err := user.SetDatabaseAccess(ctx, database, driver.Grant(permission.Grant)); err != nil {
-				return err
+				return fmt.Errorf("failed to set database access for the user: %w", err)
 			}
 		} else {
 			col, err := database.Collection(ctx, permission.Collection)
 			if err != nil {
-				return err
+				return fmt.Errorf("failed to read collection data: %w", err)
 			}
 
 			if err := user.SetCollectionAccess(ctx, col, driver.Grant(permission.Grant)); err != nil {
-				return err
+				return fmt.Errorf("failed to set collection access for the user: %w", err)
 			}
 		}
 	}
 
 	return nil
+}
+
+type CookieConnection struct {
+	driver.Connection
+	cookie string
+	mutex  sync.RWMutex
+}
+
+func (cc *CookieConnection) Do(ctx context.Context, req driver.Request) (driver.Response, error) {
+	cc.mutex.RLock()
+	if len(cc.cookie) > 0 {
+		req.SetHeader("Cookie", cc.cookie)
+	}
+	cc.mutex.RUnlock()
+	res, err := cc.Connection.Do(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	cookie := res.Header("Set-Cookie")
+	if len(cookie) > 0 {
+		cc.mutex.Lock()
+		cc.cookie = cookie
+		cc.mutex.Unlock()
+	}
+	return res, nil
+}
+
+func (cc *CookieConnection) SetAuthentication(auth driver.Authentication) (driver.Connection, error) {
+	conn, err := cc.Connection.SetAuthentication(auth)
+	if err != nil {
+		return nil, err
+	}
+	return &CookieConnection{Connection: conn}, nil
 }
